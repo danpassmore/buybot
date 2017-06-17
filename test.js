f146b2ff9c8f29eb88fce8781fff4c87d96f0807
Dan Passmore <dpassmor@TheSource.local>
2017-06-16 21:42:06 -0700

    Upload of initial scripts that mess around with gdax api

---
+++ test.js
@@ -0,0 +1,45 @@
+var gdax = require('gdax');
+var util = require('util');
+var publicClient = new gdax.PublicClient('ETH-USD', 'https://api.gdax.com');
+var sandboxClient = new gdax.PublicClient('ETH-USD', 'https://api-public.sandbox.gdax.com');
+
+var generic_handler = function(err, response, data) {
+  var str = util.format("err %s\tresponse %s\tdata %s", err, response, data);
+
+  if (err != null) {
+    console.log('-------------------------');
+    console.log('---------ERR-------------');
+    console.log('-------------------------');
+    console.log(err);
+    console.log('-------------------------');
+  }
+
+  if (true && data != null) {
+    console.log('-------------------------');
+    console.log('---------data------------');
+    console.log('-------------------------');
+    console.log(data);
+    console.log('-------------------------');
+  }
+};
+
+//console.log('=========================');
+//publicClient.getProductOrderBook({'level': 2}, generic_handler);
+//console.log('=========================');
+//publicClient.getProductTicker(generic_handler);
+//console.log('=========================');
+//publicClient.getProductTrades(generic_handler);
+//console.log('=========================');
+var time_handler = function(err, response, data){
+
+  var date = new Date(data.iso);
+  date.setDate(date.getDate() - 50);
+
+  console.log(data);
+
+  publicClient.getProductHistoricRates({'start': date.toISOString(), 'end': data.iso, 'granularity': 24*3600}, generic_handler);
+}
+
+//publicClient.getCurrencies(generic_handler);
+publicClient.getTime(time_handler);
+//publicClient.getProduct24HrStats(generic_handler);
